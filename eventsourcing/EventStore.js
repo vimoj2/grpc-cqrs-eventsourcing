d7698fa847ba113bea8352e94089c10012cc9038
Eugene Zaidun <y.zaidun@zoover.nl>
2018-09-10 23:17:31 +0200

    packing with docker

--- eventsourcing/EventStore.js
+++ eventsourcing/EventStore.js
@@ -1,51 +1,53 @@
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
-const utils = require('serializer');
-
-const RPC_HOST = 'localhost:28888';
-const PROTO_PATH = '../proto/eventstore.proto';
 const StreamStore = require('./StreamStore').StreamStore;
-const streamStore = new StreamStore();
+const config = require('./config');
+
+const RPC_HOST = `${config.host}:${config.port}`;
+const PROTO_PATH = '../proto/eventstore.proto';
+
 
 const log = console.log;
 
-const getEvents = (call, callback) => {
-  callback(null, streamStore.getStreamEvents(call.request.streamId))
-};
-
-const setEvents = (call, callback) => {
-  const { streamId, events } = call.request;
-  callback(null, streamStore.write(streamId, events));
-};
-
-const subscribe = (call, callback) => {
-  const { projection } = call.request;
-  log('[Subscriber]', projection);
-
-  const subscriberId = call.metadata.getMap()['client'];
-  call.on('cancelled', () => {
-    streamStore.unsubscribe(subscriberId);
-  });
-
-  streamStore
-    .createProjection(projection, subscriberId)
-    .onValue((events) => {
-      call.write({ events });
-    });
-};
-
-const getInfo = (call, callback) => {
-  callback(null, streamStore.getInfo());
-};
-
-const handlers = {
-  getEvents,
-  setEvents,
-  subscribe,
-  getInfo
-};
-
 function main(root) {
+
+  const getEvents = (call, callback) => {
+    callback(null, streamStore.getStreamEvents(call.request.streamId))
+  };
+
+  const setEvents = (call, callback) => {
+    const { streamId, events } = call.request;
+    callback(null, streamStore.write(streamId, events));
+  };
+
+  const subscribe = (call, callback) => {
+    const { projection } = call.request;
+    log('[Subscriber]', projection);
+
+    const subscriberId = call.metadata.getMap()['client'];
+    call.on('cancelled', () => {
+      streamStore.unsubscribe(subscriberId);
+    });
+
+    streamStore
+      .createProjection(projection, subscriberId)
+      .onValue((events) => {
+        call.write({ events });
+      });
+  };
+
+  const getInfo = (call, callback) => {
+    callback(null, streamStore.getInfo());
+  };
+
+  const handlers = {
+    getEvents,
+    setEvents,
+    subscribe,
+    getInfo
+  };
+
+  const streamStore = new StreamStore();
   const server = new grpc.Server();
   server.addService(root.Eventstore.service, handlers);
   const credentials = grpc.ServerCredentials.createInsecure();
@@ -54,10 +56,16 @@ function main(root) {
   log('[RPC server initialised]');
 }
 
-//preparing network i/o
-protoLoader
-  .load(PROTO_PATH)
-  .then(packageDefinition => (
-    grpc.loadPackageDefinition(packageDefinition).zoover
-  ))
-  .then(main);
+module.exports = () => {
+  //preparing network i/o
+  protoLoader
+    .load(PROTO_PATH)
+    .then(packageDefinition => (
+      grpc.loadPackageDefinition(packageDefinition).zoover
+    ))
+    .then(main);
+};
+
+if (module === require.main) {
+  module.exports();
+}
